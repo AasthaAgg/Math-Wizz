6fad0018e9d746339de560ec4f896eb75d31f411
Aastha Aggarwal <aastha.7090@gmail.com>
2022-09-27 16:38:37 +0530

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
 var userScore = 0;
 var generatedQues;
+var timer;
+var timerSec = 5;
+var msg = "GAME OVER !!";
+
+
+// ===== DISPLAY PAGE 2 & START GAME =====
 
 function displayPage2(){
     document.querySelector('.page1').style.display = "none";
@@ -10,35 +16,70 @@ function displayPage2(){
     generateQues();
 }
 
+
+// ===== SET USERNAME =====
+
 function setUserName(){
     var userName = document.querySelector('.userName').value;
     document.querySelector('.setUserName').innerHTML += (userName+"👋");
 }
 
+
+// ===== SET SCOREBOARD =====
+
 function setScore(){
     document.querySelector('.score').innerHTML = userScore;
 }
 
+
+// ===== GENERATE QUESTION =====
+
 function generateQues(){
     var len = userScore.toString().length;
+    timerSec = timerSec*len;
     generatedQues = "";
 
     const numbers = "0123456789";
     const symbols = "+-*/";
+    var lenNum1 = 1;
+    var lenNum2 = 1;
 
-    var i=len;
-    while(i>0){
-       generatedQues += getNumber();
-        i--;
+    // SET DIFFICULTY BY CHANGING LENGTH OF NUMBERS
+
+    if(len > 1){
+        if(len%2 == 0){
+            lenNum1++;
+        }else{
+            lenNum2++;
+        }
     }
+
+    // GENERATE 1st NUMBER
+
+    for(var i=0;i<lenNum1;i++){
+       generatedQues += getNumber();
+    }
+
+    // GENERATE SYMBOL
 
     generatedQues += getSymbol();
 
-    var j=len;
-    while(j>0){
-       generatedQues += getNumber();
-        j--;
-    }
+    // GENERATE 2nd NUMBER
+
+    for(var i=0;i<lenNum2;i++){
+
+        // TO AVOID DIVIDE BY ZERO
+        if(lenNum2 == 1 && generatedQues.charAt(generatedQues.length-1) === '/'){
+            var tempNum2 = getNumber();
+            while(tempNum2 == 0){
+                tempNum2 = getNumber();
+            }
+            generatedQues += tempNum2;
+        }
+        else{
+            generatedQues += getNumber();
+        }
+     }
 
     function getSymbol(){
         return symbols[Math.floor(Math.random()*symbols.length)];
@@ -51,9 +92,31 @@ function generateQues(){
     setQues();
 }
 
+// ===== SET QUESTION ON SCREEN & START TIMER =====
+
 function setQues(){
     document.querySelector('.ques').innerHTML = generatedQues;
+    clearTimeout(timer);
+    countDown(timerSec);
 }
+
+// ====== TIMER SETTINGS =====
+
+function countDown(secs) {
+	document.querySelector('.timer').innerHTML = "Time Left :  " + secs + " secs";
+
+	if(secs < 1) {
+		clearTimeout(timer);
+		msg = "OOPs, TIME's UP!!";
+        setMsgScoreOfPage3();
+        displayPage3();
+	}
+
+	secs--;
+	timer = setTimeout('countDown('+secs+')',1000);
+}
+
+// ===== CHECK USER INPUT =====
 
 function checkResult(){
     var ans = parseInt(document.querySelector('.ans').value);
@@ -63,30 +126,41 @@ function checkResult(){
         resetAns();
         generateQues();
     }else{
-        displayEndWin();
+        setMsgScoreOfPage3();
+        displayPage3();
     }
 }
 
-function displayEndWin(){
-    
-    document.querySelector('.page2').style.display = "none";
-	document.querySelector('.page3').style.display = "flex";
-    setEndPage();
-}
+// ===== GET CORRECT ANS =====
 
 function evaluateAns(ques){
     return parseInt(eval(ques));
 }
+
+// ===== UPDATE SCORE =====
 
 function updateScore(){
     userScore++;
     setScore();
 }
 
+// ===== RESET INPUT SECTION FOR NEXT ANSWER =====
+
 function resetAns(){
     document.querySelector('.ans').value = "";
 }
 
-function setEndPage(){
-    document.querySelector('.displayScore').innerHTML += (userScore+" ✨");
+
+// ===== DISPLAY END PAGE =====
+
+function displayPage3(){
+    document.querySelector('.page2').style.display = "none";
+	document.querySelector('.page3').style.display = "flex";
+}
+
+// ===== SET DETAILS ON END PAGE =====
+
+function setMsgScoreOfPage3(){
+    document.querySelector('.msg').innerHTML = msg;
+    document.querySelector('.displayScore').innerHTML = "Score : " + userScore + " ✨";
 }
